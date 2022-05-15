c83ce89442c1e85406d9d156daae01147cf75b6e
Dobry-kot <dlputilin@dobry-kot.ru>
2022-05-15 05:57:40 +0300

    Добавляет статик поды etcd,kcm,kapi исправляет ошибки сертификатов etcd

--- helpers.md
+++ helpers.md
@@ -1,14 +1,14 @@
 
 
-HOST_1=cl11erbm03uhlfvrl4tp-uzav
-HOST_2=cl11erbm03uhlfvrl4tp-yxox
-HOST_3=cl11erbm03uhlfvrl4tp-ufow
+HOST_1=cl15tk4od57v58kegou7-esac
+HOST_2=cl15tk4od57v58kegou7-uvuh
+HOST_3=cl15tk4od57v58kegou7-aqop
 ENDPOINTS=$HOST_1:2379,$HOST_2:2379,$HOST_3:2379
 etcdctl \
 --write-out=table \
 --endpoints=$ENDPOINTS \
---cert /pki/certs/etcd/system:etcd-peer.pem \
---key /pki/certs/etcd/system:etcd-peer-key.pem \
---cacert /pki/ca/etcd-ca.pem \
+--cert /etc/kubernetes/pki/certs/etcd/system:etcd-peer.pem \
+--key /etc/kubernetes/pki/certs/etcd/system:etcd-peer-key.pem \
+--cacert /etc/kubernetes/pki/ca/etcd-ca.pem \
 endpoint status
 
--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,13 +1,11 @@
-HOST1=51.250.87.176
+HOST1=51.250.14.5
 HOST2=51.250.70.241
-HOST3=51.250.77.118
+HOST3=51.250.78.145
 
-# ssh dkot@$HOST1 'sudo rm -rf /etc/kubernetes/pki/certs/etcd/system\:etcd-server*'
-# ssh dkot@$HOST2 'sudo rm -rf /etc/kubernetes/pki/certs/etcd/system\:etcd-server*'
-# ssh dkot@$HOST3 'sudo rm -rf /etc/kubernetes/pki/certs/etcd/system\:etcd-server*'
+ssh dkot@$HOST1 'sudo rm -rf /etc/kubernetes/manifests/*'
+ssh dkot@$HOST2 'sudo rm -rf /etc/kubernetes/manifests/*'
+ssh dkot@$HOST3 'sudo rm -rf /etc/kubernetes/manifests/*'
 
-
-
-ssh dkot@$HOST1 'sudo systemctl stop etcd'
-ssh dkot@$HOST2 'sudo systemctl stop etcd'
-ssh dkot@$HOST3 'sudo systemctl stop etcd'
+ssh dkot@$HOST1 'sudo shutdown -r now'
+ssh dkot@$HOST2 'sudo shutdown -r now'
+ssh dkot@$HOST3 'sudo shutdown -r now'
