02da25b8c2c3bbbf6138c1bcfc495d11b3f61b28
Dobry-kot <dlputilin@dobry-kot.ru>
2022-07-07 01:42:01 +0300

    Разделяет на отдельные блоки тф. выносит создание рса в тф. исправлены конфиги адверс апи

--- helpers.md
+++ helpers.md
@@ -1,8 +1,129 @@
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+helm repo add cilium https://helm.cilium.io/
+helm repo add coredns https://coredns.github.io/helm
+
+crictl --runtime-endpoint unix:///run/containerd/containerd.sock ps -a
+export KUBECONFIG=/etc/kubernetes/kube-apiserver/kubeconfig
+kubectl get no
+
+container=$(crictl --runtime-endpoint unix:///run/containerd/containerd.sock ps -a | grep cilium-agent | awk '{print $1}')
+PID=$(crictl inspect $container | grep pid )
+nsenter --target    1839   --mount --uts   --ipc   --net   --pid
 
 
-HOST_1=firecube-master-0.example.com
-HOST_2=firecube-master-1.example.com
-HOST_3=firecube-master-2.example.com
+helm upgrade --install --namespace kube-system cilium cilium/cilium  --version 1.11.6 \
+--values - <<EOF
+kubeProxyReplacement: strict
+k8sServiceHost: api.cluster-1.dobry-kot.ru
+k8sServicePort: 6443
+
+ipam:
+  mode: "cluster-pool"
+  operator:
+    clusterPoolIPv4PodCIDR: 10.220.2.0/16
+    clusterPoolIPv4MaskSize: 26
+
+containerRuntime:
+  integration: containerd
+  socketPath: /run/containerd/containerd.sock
+
+cluster:
+  name: cluster-1
+  id: 11
+
+hubble:
+  enabled: true
+  relay:
+    enabled: true
+  ui:
+    enabled: true
+    tls:
+      enabled: true
+      auto:
+        enabled: true
+l2NeighDiscovery:
+  enabled: false
+tunnel: vxlan
+prometheus:
+  enabled: true
+  port: 9090
+proxy:
+  prometheus:
+    enabled: true
+    port: 9095
+wellKnownIdentities:
+  enabled: true
+enableK8sEventHandover: true
+endpointRoutes:
+  enabled: true 
+autoDirectNodeRoutes: false
+enableIPv4Masquerade: true
+hostPort:
+  enabled: true
+hostServices:
+    enabled: true
+    hostNamespaceOnly: true
+EOF
+
+helm upgrade coredns coredns/coredns \
+    --install \
+    --namespace=kube-system \
+    --set service.clusterIP="29.64.0.10" \
+    --set replicaCount=3
+
+alias kn='kubectl config set-context --current '
+alias kg='kubectl get'
+alias ka='kubectl apply'
+alias kd='kubectl delete'
+alias ki='kubectl describe'
+alias ke='kubectl edit'
+alias k='kubectl '
+alias kl='kubectl logs '
+alias ees='etcd_endpoints '
+alias cll='kubectl config get-contexts'
+alias cle='kubectl config use-context '
+
+
+cat <<EOF > ~/.kube/config
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority: /etc/kubernetes/pki/ca/root-ca.pem
+    server: https://api.cluster-1.dobry-kot.ru:6443
+  name: pfm-stage-1
+- cluster:
+    certificate-authority: /etc/kubernetes/pki/ca/root-ca2.pem
+    server: https://api.cluster-2.dobry-kot.ru:6443
+  name: pfm-stage-2
+contexts:
+- context:
+    cluster: pfm-stage-1
+    namespace: kube-system
+    user: kubernetes-admin-1
+  name: kubernetes-admin-1
+- context:
+    cluster: pfm-stage-2
+    namespace: kube-system
+    user: kubernetes-admin-2
+  name: kubernetes-admin-2
+current-context: kubernetes-admin-1
+kind: Config
+preferences: {}
+users:
+- name: kubernetes-admin-1
+  user:
+    client-certificate: /etc/kubernetes/pki/certs/kube-apiserver/system:kube-apiserver-kubelet-client.pem
+    client-key: /etc/kubernetes/pki/certs/kube-apiserver/system:kube-apiserver-kubelet-client-key.pem
+- name: kubernetes-admin-2
+  user:
+    client-certificate: /etc/kubernetes/pki/certs/kube-apiserver/system:kube-apiserver-kubelet-client2.pem
+    client-key: /etc/kubernetes/pki/certs/kube-apiserver/system:kube-apiserver-kubelet-client-key2.pem
+EOF
+
+
+HOST_1=master-0.cluster-1.dobry-kot.ru
+HOST_2=master-1.cluster-1.dobry-kot.ru
+HOST_3=master-2.cluster-1.dobry-kot.ru
 
 ENDPOINTS=$HOST_1:2379,$HOST_2:2379,$HOST_3:2379
 etcdctl \
---
+++ terraform/k8s/main.tf
@@ -0,0 +1,134 @@
+
+#### VPC ######
+##-->
+resource "yandex_vpc_network" "cluster-vpc" {
+  name = "vpc.${var.cluster_name}"
+}
+
+#### SUBNETS ######
+##-->
+resource "yandex_vpc_subnet" "cluster-subnet" {
+  for_each        = "${var.yc_availability_master_zones}"
+  v4_cidr_blocks  = [each.value]
+  zone            = "${each.key}"
+  network_id      = "${yandex_vpc_network.cluster-vpc.id}"
+  name            = "vpc-${each.key}-${var.cluster_name}" 
+}
+
+#### DNS ######
+##-->
+resource "yandex_dns_zone" "zone1" {
+  name        = "dns-zone-${var.cluster_name}"
+  description = "desc"
+  zone             = "${var.cluster_name}.${var.base_domain}."
+  public           = false
+  private_networks = [yandex_vpc_network.cluster-vpc.id]
+}
+
+resource "yandex_dns_recordset" "rs1" {
+  for_each    = "${var.yc_availability_master_zones}"
+  zone_id = yandex_dns_zone.zone1.id
+  name    = "master-${index(keys(var.yc_availability_master_zones), each.key)}.${var.cluster_name}.${var.base_domain}."
+  type    = "A"
+  ttl     = 60
+  data    = ["${yandex_compute_instance.master[each.key].network_interface.0.ip_address}"]
+}
+
+resource "yandex_dns_recordset" "api" {
+  zone_id = yandex_dns_zone.zone1.id
+  name    = "api.${var.cluster_name}.${var.base_domain}."
+  type    = "A"
+  ttl     = 60
+  data    = "${(tolist(yandex_lb_network_load_balancer.master-lb.listener)[0].external_address_spec)[*].address}"
+}
+
+
+
+#### MASTERS ######
+##-->
+resource "yandex_compute_instance" "master" {
+
+  for_each    = "${var.yc_availability_master_zones}"
+  name        = "master-${index(keys(var.yc_availability_master_zones), each.key)}-${var.cluster_name}"
+  platform_id = "standard-v1"
+  zone        = "${each.key}"
+
+  resources {
+    cores         = "${var.master_flavor.core}"
+    memory        = "${var.master_flavor.memory}"
+    core_fraction = "${var.master_flavor.core_fraction}"
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "${var.base_os_image}"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.cluster-subnet[each.key].id}"
+    nat = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    user-data = templatefile("templates/cloud-init.tftpl", { 
+        temporary_token = "${vault_approle_auth_backend_login.login.client_token}",
+        cluster_name    = "${var.cluster_name}",
+        base_domain     = "${var.base_domain}",
+        # certs           = "${local.ssl}",
+        ssh_key         = "${file("~/.ssh/id_rsa.pub")}"
+        list_masters    = "${local.list_masters}"
+        etcd_advertise_client_urls = "${local.etcd_advertise_client_urls}"
+        instance_name   = "master-${index(keys(var.yc_availability_master_zones), each.key)}.${var.cluster_name}"
+        # lb_api_ip       = "${yandex_lb_network_load_balancer.master-lb.listener}"
+        etcd_initial_cluster = "${local.etcd_initial_cluster}"
+        })
+  }
+}
+
+#### LB ######
+##-->
+resource "yandex_lb_target_group" "master-tg" {
+  name        = "master-target-group-${var.cluster_name}"
+  region_id   = "ru-central1"
+
+  dynamic "target" {
+    for_each = "${var.yc_availability_master_zones}"
+    content {
+      subnet_id = "${yandex_vpc_subnet.cluster-subnet[target.key].id}"
+      address   = "${yandex_compute_instance.master[target.key].network_interface.0.ip_address}"
+    }
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "master-lb" {
+  name = "lb-api-${var.cluster_name}"
+  listener {
+    name = "api-listener-${var.cluster_name}"
+    port = 6443
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+  attached_target_group {
+    target_group_id = "${yandex_lb_target_group.master-tg.id}"
+
+    healthcheck {
+      name = "tcp"
+      tcp_options {
+        port = 6443
+
+      }
+    }
+  }
+}
+
+output "cloud_init" {
+    value = "${yandex_compute_instance.master[*].ru-central1-a.network_interface[*].nat_ip_address[*]}"
+}
+
+
+output "LB-API" {
+    value = "${tolist(tolist(yandex_lb_network_load_balancer.master-lb.listener)[0].external_address_spec)[0].address}"
+}
